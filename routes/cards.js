0b8d0f83b44e68f0091bdd53516f5c6cd5cc72f1
Алекандра Фомакина <alekandrafomakina@MacBook-Air-Alekandra.local>
2023-10-17 22:18:24 +0500

    completed back without checks9

--- routes/cards.js
+++ routes/cards.js
@@ -12,7 +12,7 @@ const {
 
 router.get('/', getCards);
 
-router.post('/cards', celebrate({
+router.post('/', celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
     link: Joi.string().required().pattern(URL_VALIDATION),
